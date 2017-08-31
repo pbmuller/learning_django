94527a76ee27a555ea9b388f234e0a3079a09bbf
pbmuller <muller.98@osu.edu>
2017-08-31 16:57:02 -0400

    update views. Add template dir

--- polls/models.py
+++ polls/models.py
@@ -17,7 +17,7 @@ class Question(models.Model):
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Answer(models.Model):
+class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
--- polls/views.py
+++ polls/views.py
@@ -6,4 +6,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, World! You are at the Polls index")
+	lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in lastest_question_list])
+	return HttpResponse(ouput)
+
+def detail(request, question_id):
+	return HttpResponse("You're looking at question %s" %question_id)
+
+def results(request, question_id):
+	response = "You're looking at the results of questions %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
